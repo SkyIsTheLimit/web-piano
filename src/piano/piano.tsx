bd0711a78f0cae746b8d4ee1168ece71d5cd33df
Sandeep Prasad <s3g007@gmail.com>
2022-03-29 18:05:59 -0400

    added stitches around the power button.

--- src/piano/piano.tsx
+++ src/piano/piano.tsx
@@ -147,38 +147,41 @@ export class Piano extends Component<
           </div>
         </div>
 
-        <div className='absolute top-2 left-8 w-fit h-fit flex'>
-          <div className='relative mr-0 bg-transparent rounded-md py-2 px-4 bg-slate-700 flex justify-between items-center'>
-            <button
-              className={`${styles.powerButton} ${
-                this.state.power ? styles.pressed : ''
-              }`}
-              onClick={() => this.togglePower()}
-            >
-              <svg viewBox='0 0 30.143 30.143' className='w-4'>
-                <g>
-                  <path
-                    fill='#030104'
-                    d='M20.034,2.357v3.824c3.482,1.798,5.869,5.427,5.869,9.619c0,5.98-4.848,10.83-10.828,10.83 c-5.982,0-10.832-4.85-10.832-10.83c0-3.844,2.012-7.215,5.029-9.136V2.689C4.245,4.918,0.731,9.945,0.731,15.801 c0,7.921,6.42,14.342,14.34,14.342c7.924,0,14.342-6.421,14.342-14.342C29.412,9.624,25.501,4.379,20.034,2.357z'
-                  />
-                  <path
-                    fill='#030104'
-                    d='M14.795,17.652c1.576,0,1.736-0.931,1.736-2.076V2.08c0-1.148-0.16-2.08-1.736-2.08 c-1.57,0-1.732,0.932-1.732,2.08v13.496C13.062,16.722,13.225,17.652,14.795,17.652z'
-                  />
-                </g>
-              </svg>
-            </button>
-            {this.state.power ? (
-              <span
-                className='rounded-full inline-block mx-4 w-[0.8em] h-[0.8em] bg-green-400'
-                style={{ boxShadow: '0 0 0.6em 0.2em rgb(134, 239, 172)' }}
-              ></span>
-            ) : (
-              <span
-                className='rounded-full inline-block mx-4 w-[0.8em] h-[0.8em] bg-red-400'
-                style={{ boxShadow: '0 0 0.6em 0.2em rgb(239, 134, 172)' }}
-              ></span>
-            )}
+        <div className='absolute top-1 left-8 w-fit h-fit flex'>
+          <div className='relative mr-0 bg-transparent rounded-md p-1 bg-slate-700 flex justify-between items-center'>
+            <div className='border border-dashed border-slate-600 py-2 px-4 rounded-md'>
+              <button
+                className={`${styles.powerButton} ${
+                  this.state.power ? styles.pressed : ''
+                }`}
+                onClick={() => this.togglePower()}
+              >
+                <svg viewBox='0 0 30.143 30.143' className='w-3'>
+                  <g>
+                    <path
+                      fill='#030104'
+                      d='M20.034,2.357v3.824c3.482,1.798,5.869,5.427,5.869,9.619c0,5.98-4.848,10.83-10.828,10.83 c-5.982,0-10.832-4.85-10.832-10.83c0-3.844,2.012-7.215,5.029-9.136V2.689C4.245,4.918,0.731,9.945,0.731,15.801 c0,7.921,6.42,14.342,14.34,14.342c7.924,0,14.342-6.421,14.342-14.342C29.412,9.624,25.501,4.379,20.034,2.357z'
+                    />
+                    <path
+                      fill='#030104'
+                      d='M14.795,17.652c1.576,0,1.736-0.931,1.736-2.076V2.08c0-1.148-0.16-2.08-1.736-2.08 c-1.57,0-1.732,0.932-1.732,2.08v13.496C13.062,16.722,13.225,17.652,14.795,17.652z'
+                    />
+                  </g>
+                </svg>
+              </button>
+
+              {this.state.power ? (
+                <span
+                  className='rounded-full inline-block ml-4 mr-1 w-[0.8em] h-[0.8em] bg-green-400'
+                  style={{ boxShadow: '0 0 0.6em 0.2em rgb(134, 239, 172)' }}
+                ></span>
+              ) : (
+                <span
+                  className='rounded-full inline-block ml-4 mr-1 w-[0.8em] h-[0.8em] bg-red-400'
+                  style={{ boxShadow: '0 0 0.6em 0.2em rgb(239, 134, 172)' }}
+                ></span>
+              )}
+            </div>
           </div>
         </div>
         {([0, 1, 2, 3, 4, 5, 6, 7, 8] as KeyboardRegister[]).map(
